a7244e02ddbfba1c80b425526559dd1fb9bdb38a
Jospaul Mahajan Prakash <iamjospaul@gmail.com>
2023-03-10 11:28:55 -0600

    Removed company folder and references

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI, Depends
 import models
 from database import engine
 from routers import auth, todos, users, address
-from company import companyapi, dependencies
 
 app = FastAPI()
 
@@ -12,10 +11,3 @@ app.include_router(auth.router)
 app.include_router(todos.router)
 app.include_router(users.router)
 app.include_router(address.router)
-app.include_router(
-    companyapi.router,
-    prefix = "/companyapis",
-    tags=["companyapis"],
-    dependencies=[Depends(dependencies.get_token_header)],
-    responses={418:{"description": "Internal Use Only"}}
-)
