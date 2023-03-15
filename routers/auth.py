252e16ca1cd0045c8ad230382e39ab976d1bb4a4
Jospaul Mahajan Prakash <iamjospaul@gmail.com>
2023-03-15 14:11:34 -0500

    Code cleanup 2

--- routers/auth.py
+++ routers/auth.py
@@ -90,7 +90,7 @@ async def get_current_user(request:Request):
             logout(request)
         return {"username": username, "id": user_id}
     except JWTError:
-        raise HTTPException(status_code=404, detail="Not Found")
+        raise get_user_exception()
 
 
 @router.post("/token")
@@ -167,3 +167,19 @@ async def register_user(request: Request, email: str = Form(...), username: str 
     msg = "User successfully created"
     return templates.TemplateResponse("login.html", {"request": request, "msg": msg})
 
+#Exceptions
+def get_user_exception():
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail = "Could not validate credentials.",
+        headers = {"WWW-Authenticate": "Bearer"}
+    )
+    return credentials_exception
+
+def token_exception():
+    token_exception_response = HTTPException(
+        status_code = status.HTTP_401_UNAUTHORIZED,
+        detail = "Incorrect username or password.",
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+    return token_exception_response
