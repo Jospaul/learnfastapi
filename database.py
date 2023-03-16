bd02a37ae2c7228fff65b47dd0d7de5f7a519ec9
Jospaul Mahajan Prakash <iamjospaul@gmail.com>
2023-03-16 15:31:30 -0500

    Production database change

--- database.py
+++ database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgresUser:postgresPW@localhost/TodoApplicationDatabase"
+#SQLALCHEMY_DATABASE_URL = "postgresql://postgresUser:postgresPW@localhost/TodoApplicationDatabase"
+SQLALCHEMY_DATABASE_URL = "postgresql://cuulicqp:y5ggwhZWuCQM32zb3ZhooLP-Gc65pevh@mahmud.db.elephantsql.com/cuulicqp"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
